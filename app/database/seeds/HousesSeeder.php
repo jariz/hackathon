65c0bf8f2765b5970a48cedc449a14a74825206c
JariZ <the@jariz.nl>
2014-05-17 01:38:01 +0200

    HousesSeeder!!!!

--- app/database/seeds/HousesSeeder.php
+++ app/database/seeds/HousesSeeder.php
@@ -9,7 +9,30 @@
 class HousesSeeder extends Seeder{
 
     public function run(){
-        // Populate houses table
+        $this->command->info("Parsing JSON...");
+        $data = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', File::get("data/real_estate.json")));
+        $this->command->info("Looping...");
+        $valid = 0;
+        $pad = "http://huizenzoeker.nl";
+        foreach($data->items as $item) {
+            if(substr($item->url, 6, 23) != "noord-holland/amsterdam") continue;
+            $valid++;
+            $house = new House();
+            $house->adres = $item->adres;
+            if(@$item->postcode)
+                $house->postal_code = $item->postcode;
+            if(@$item->prijs)
+                $house->price = intval(preg_replace("/[^0-9]/","", $item->prijs));
+            $house->url = $pad.$item->url;
+            $house->photo = $pad.$item->foto;
+            if(@$item->prijssoort)
+                $house->price_sort = $item->prijssoort;
+            $house->lat = $item->lat;
+            $house->lng = $item->lng;
+            $house->house_id = $item->nr;
+            $house->save();
+        }
+        $this->command->info("Real estate in Amsterdam: {$valid} Real estate total: ".count($data->items));
     }
 
 } 
