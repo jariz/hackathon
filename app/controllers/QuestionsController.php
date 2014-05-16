7635d0087666acdbd82580e2679d5bfd3dec4b5c
ssshenkie <crashbandie@hotmail.com>
2014-05-17 01:30:33 +0200

    questions bug fixed, controller not found

--- app/controllers/QuestionsController.php
+++ app/controllers/QuestionsController.php
@@ -3,11 +3,13 @@
  * Created by PhpStorm.
  * User: ssshenkie
  * Date: 5/17/14
- * Time: 1:09 AM
+ * Time: 1:29 AM
  */
 
-class QuestionController extends BaseController{
-    public function show() {
+class QuestionsController {
+
+    public function show(){
         return View::make("questions");
     }
+
 } 
