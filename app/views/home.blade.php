a0498680844aebc5ec72d147dcae36b7dfc703b9
Djilano Smit <djilano.smit@gmail.com>
2014-05-17 14:08:15 +0200

    - logo responsive * negers doen niet mee aan fexibiliteit zoals .logo #jwz

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <header>
 	<div class="container">
-		<img src="images/logo.png" class="wow fadeInDown" data-wow-delay="1s">
+		<img src="images/logo.png" class="wow fadeInDown logo" data-wow-delay="1s">
 		<h2 class="wow fadeInUp" data-wow-delay="1s">Find yourself a house that fits your interests!</h2>
 		<em class="wow fadeInUp" data-wow-delay="1s">Based on a couple simple questions</em>
 		<a href="{{URL::route('questions')}}" class="wow fadeInUp button" data-wow-delay="1s">Let's start!</a>
